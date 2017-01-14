0bc30e3cca479dfd5ae7fc50d2e150799024078b
fanda <fandazeng@gmail.com>
2017-01-14 16:58:10 +0800

    增加百分比布局

---
+++ app/src/main/java/com/fanda/zeng/bookpratice/UI/UISecondActivity.java
@@ -0,0 +1,31 @@
+package com.fanda.zeng.bookpratice.UI;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fanda.zeng.bookpratice.R;
+import com.fanda.zeng.bookpratice.activity.BaseActivity;
+
+/**
+ * Created by zfd on 2017/1/13 0013.]
+ * 快捷技巧演示类
+ */
+
+public class UISecondActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_second2);
+
+        initView();
+
+    }
+
+    private void initView() {
+
+
+    }
+}
