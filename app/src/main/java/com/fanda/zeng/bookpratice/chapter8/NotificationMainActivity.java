19d1df1dd70e8922bc720a18dc8959ef844359c4
fanda <fandazeng@gmail.com>
2017-03-27 01:53:25 +0800

    增加布局动画，drawable

--- app/src/main/java/com/fanda/zeng/bookpratice/chapter8/NotificationMainActivity.java
+++ app/src/main/java/com/fanda/zeng/bookpratice/chapter8/NotificationMainActivity.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
